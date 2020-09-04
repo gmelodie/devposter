8d6ce2ea0155a413892c169eeabb488c14ca510f
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2020-09-04 16:42:09 +0200

    Add working test

--- test.py
+++ test.py
@@ -1,21 +1,38 @@
-import concurrent.futures
-import datetime
+import threading, queue, datetime, time
 
-def scheduler(timestamps_pool):
+q = queue.PriorityQueue()
+
+def poster():
     while True:
-        now = datetime.datetime.now().timestamp()
-        if timestamps_pool[0] <= now():
-            timestamp = timestamps_pool.pop(0)
-            fp = open(str(timestamp), "w+")
-            fp.close()
+        timestamp = q.get()
+        now = datetime.datetime.utcnow().timestamp()
 
+        print(f"Trying {timestamp} at {now}")
 
-timestamps_pool = []
+        if timestamp <= now:
+            print(f'Finished {timestamp}')
+            q.task_done()
+        else:
+            q.put(timestamp, timestamp)
 
-with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
-    executor.submit(scheduler, timestamps_pool)
+        time.sleep(1)
 
-while True:
-    wait_seconds = int(input(f"How many seconds to wait: "))
-    now = datetime.datetime.now().timestamp()
-    timestamps_pool.append(now + wait_seconds)
+# turn-on the worker thread
+threading.Thread(target=poster, daemon=True).start()
+
+now = datetime.datetime.utcnow().timestamp()
+print(f"Now: {now}")
+
+timestamp1 = now + 10
+timestamp2 = now + 2
+timestamp3 = now + 5
+
+q.put(timestamp1, timestamp1)
+q.put(timestamp2, timestamp2)
+q.put(timestamp3, timestamp3)
+
+print('All task requests sent\n', end='')
+
+# block until all tasks are done
+q.join()
+print('All work completed')
